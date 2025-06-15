98c3b837f1b6b521df8242876617a8b70f5adf05
Feekkk <zxduke350@gmail.com>
2025-06-15 10:13:01 +0800

    add event and assign button

---
+++ lib/admin/pages/add_assign.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+
+class AddAssign extends StatelessWidget {
+  const AddAssign({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Assign Member'),
+      ),
+      body: const Center(
+        child: Text('Assign Member Page'),
+      ),
+    );
+  }
+}
--- lib/admin/tabs/timetable_tab.dart
+++ lib/admin/tabs/timetable_tab.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:calendar_date_picker2/calendar_date_picker2.dart';
+import '../pages/add_event.dart';
+import '../pages/add_assign.dart';
 
 class TimetableTab extends StatefulWidget {
   const TimetableTab({super.key});
@@ -62,32 +64,91 @@ class _TimetableTabState extends State<TimetableTab> {
             ),
           ),
           const SizedBox(height: 24),
-          // Space for future functionality
           Expanded(
-            child: Container(
-              decoration: BoxDecoration(
-                color: Colors.white,
-                borderRadius: BorderRadius.circular(16),
-                boxShadow: [
-                  BoxShadow(
-                    color: Colors.black.withOpacity(0.1),
-                    blurRadius: 10,
-                    offset: const Offset(0, 4),
-                  ),
-                ],
-              ),
-              child: const Center(
-                child: Text(
-                  'Additional functionality will be added here',
-                  style: TextStyle(
-                    color: Colors.grey,
-                    fontSize: 16,
+            child: Row(
+              children: [
+                Expanded(
+                  child: _buildActionCard(
+                    context,
+                    'Add Event',
+                    Icons.event,
+                    Colors.blue,
+                    () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AddEvent()),
+                    ),
                   ),
                 ),
-              ),
+                const SizedBox(width: 16),
+                Expanded(
+                  child: _buildActionCard(
+                    context,
+                    'Assign Member',
+                    Icons.person_add,
+                    Colors.green,
+                    () => Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => const AddAssign()),
+                    ),
+                  ),
+                ),
+              ],
             ),
           ),
         ],
+      ),
+    );
+  }
+
+  Widget _buildActionCard(
+    BuildContext context,
+    String title,
+    IconData icon,
+    Color color,
+    VoidCallback onTap,
+  ) {
+    return Card(
+      elevation: 4,
+      shape: RoundedRectangleBorder(
+        borderRadius: BorderRadius.circular(16),
+      ),
+      child: InkWell(
+        onTap: onTap,
+        borderRadius: BorderRadius.circular(16),
+        child: Container(
+          padding: const EdgeInsets.all(20),
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            gradient: LinearGradient(
+              begin: Alignment.topLeft,
+              end: Alignment.bottomRight,
+              colors: [
+                color.withOpacity(0.7),
+                color,
+              ],
+            ),
+          ),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Icon(
+                icon,
+                size: 48,
+                color: Colors.white,
+              ),
+              const SizedBox(height: 16),
+              Text(
+                title,
+                style: const TextStyle(
+                  color: Colors.white,
+                  fontSize: 18,
+                  fontWeight: FontWeight.bold,
+                ),
+                textAlign: TextAlign.center,
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
