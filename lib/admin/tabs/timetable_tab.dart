9f5074384416c961a79b74481a29b553c4021711
Feekkk <zxduke350@gmail.com>
2025-06-15 09:54:14 +0800

    add calendar UI

--- lib/admin/tabs/timetable_tab.dart
+++ lib/admin/tabs/timetable_tab.dart
@@ -1,18 +1,93 @@
 import 'package:flutter/material.dart';
+import 'package:calendar_date_picker2/calendar_date_picker2.dart';
 
-class TimetableTab extends StatelessWidget {
+class TimetableTab extends StatefulWidget {
   const TimetableTab({super.key});
 
   @override
+  State<TimetableTab> createState() => _TimetableTabState();
+}
+
+class _TimetableTabState extends State<TimetableTab> {
+  List<DateTime?> _selectedDates = [];
+
+  @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text(
-        'Timetable',
-        style: TextStyle(
-          fontSize: 24,
-          fontWeight: FontWeight.bold,
-          color: Colors.white,
-        ),
+    return Container(
+      padding: const EdgeInsets.all(16),
+      child: Column(
+        children: [
+          Container(
+            decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(16),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.1),
+                  blurRadius: 10,
+                  offset: const Offset(0, 4),
+                ),
+              ],
+            ),
+            child: CalendarDatePicker2(
+              config: CalendarDatePicker2Config(
+                calendarType: CalendarDatePicker2Type.single,
+                selectedDayHighlightColor: Theme.of(context).primaryColor,
+                weekdayLabelTextStyle: const TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.bold,
+                ),
+                controlsTextStyle: const TextStyle(
+                  color: Colors.black,
+                  fontSize: 16,
+                  fontWeight: FontWeight.bold,
+                ),
+                dayTextStyle: const TextStyle(
+                  color: Colors.black87,
+                  fontWeight: FontWeight.w500,
+                ),
+                disabledDayTextStyle: const TextStyle(
+                  color: Colors.grey,
+                ),
+                selectableDayPredicate: (day) => true,
+                firstDayOfWeek: 1,
+                weekdayLabels: const ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun'],
+              ),
+              value: _selectedDates,
+              onValueChanged: (dates) {
+                setState(() {
+                  _selectedDates = dates;
+                });
+              },
+            ),
+          ),
+          const SizedBox(height: 24),
+          // Space for future functionality
+          Expanded(
+            child: Container(
+              decoration: BoxDecoration(
+                color: Colors.white,
+                borderRadius: BorderRadius.circular(16),
+                boxShadow: [
+                  BoxShadow(
+                    color: Colors.black.withOpacity(0.1),
+                    blurRadius: 10,
+                    offset: const Offset(0, 4),
+                  ),
+                ],
+              ),
+              child: const Center(
+                child: Text(
+                  'Additional functionality will be added here',
+                  style: TextStyle(
+                    color: Colors.grey,
+                    fontSize: 16,
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ],
       ),
     );
   }
